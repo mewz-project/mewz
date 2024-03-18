4acfc77ad67eb1ffa87912d0489ccafe99111840
Saza <49511480+saza-ku@users.noreply.github.com>
2024-03-18 18:02:57 +0900

    CI: add integration test (#56)
    * CI: add integration test
    
    * README: add info about integration test
    
    * lwip: update the upstream
    
    * CI: not to use timeout

---
+++ scripts/integration-test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ex
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+cd $REPO_ROOT
+
+mkdir -p build/test
+(stty -echo; sleep 4; (sleep 0.5; echo q) | telnet localhost 1234) &
+(sleep 2; curl localhost:1234) &
+./scripts/run-qemu.sh > build/test/output.txt
+
+if ! grep -q "Integration test passed" build/test/output.txt; then
+  echo "Integration Test FAILED!!"
+  echo "output:"
+  cat build/test/output.txt
+  exit 1
+fi
+
+echo -e "\nIntegration Test PASSED!!"
