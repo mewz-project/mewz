1235360bceaa08f3537c8fc46b9c27a046298bb5
Saza-ku <the.latticeheart@gmail.com>
2024-03-18 15:17:52 +0900

    CI: not to use timeout

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -4,21 +4,10 @@ set -ex
 REPO_ROOT=$(git rev-parse --show-toplevel)
 cd $REPO_ROOT
 
-with_timeout() {
-
-    time=$1
-
-    # start the command in a subshell to avoid problem with pipes
-    # (spawn accepts one command)
-    command="/bin/sh -c \"$2\""
-
-    expect -c "set echo \"-noecho\"; set timeout $time; spawn -noecho $command; expect timeout { exit 1 } eof { exit 0 }"
-
-}
-
 mkdir -p build/test
+(stty -echo; sleep 4; (sleep 0.5; echo q) | telnet localhost 1234) &
 (sleep 2; curl localhost:1234) &
-with_timeout 5 ./scripts/run-qemu.sh > build/test/output.txt
+./scripts/run-qemu.sh > build/test/output.txt
 
 if ! grep -q "Integration test passed" build/test/output.txt; then
   echo "Integration Test FAILED!!"
