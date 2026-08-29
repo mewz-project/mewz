a7618bed821b7d325ed2a89f8bfce052f676319b
Soichiro Ueda <the.latticeheart@gmail.com>
2026-08-29 17:49:32 +0900

    Use hyper

--- examples/http_client/src/main.rs
+++ examples/http_client/src/main.rs
@@ -1,19 +1,26 @@
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpStream;
+use hyper::body::HttpBody;
+use hyper::{Client, Uri};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    println!("Connecting to google.com:80...");
+    let url = "http://google.com/";
+    println!("GET {url}...");
 
-    let mut stream = TcpStream::connect("google.com:80").await?;
-    stream
-        .write_all(b"GET / HTTP/1.1\r\nHost: google.com\r\nConnection: close\r\n\r\n")
-        .await?;
+    let uri: Uri = url.parse()?;
+    let client = Client::new();
+    let mut response = client.get(uri).await?;
 
-    let mut buf = [0u8; 1024];
-    let n = stream.read(&mut buf).await?;
-    let response = String::from_utf8_lossy(&buf[..n]);
-    println!("Response:\n{}", response);
+    println!("Status: {}", response.status());
+    println!("Headers:\n{:#?}", response.headers());
+
+    let mut body = Vec::new();
+    while let Some(chunk) = response.data().await {
+        body.extend_from_slice(&chunk?);
+    }
+
+    let preview = String::from_utf8_lossy(&body);
+    let lines: Vec<&str> = preview.lines().take(10).collect();
+    println!("Body (first 10 lines):\n{}", lines.join("\n"));
 
     Ok(())
 }
